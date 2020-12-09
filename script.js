d0cfe7eda86e93c78f5c7c783ed1a0a063622195
yourdilemma <k.iankovskaia@gmail.com>
2020-12-09 11:25:14 -0500

    added functions which check current hour and change color

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+// grab save button
+let saveBtn = $(".saveBtn")
+
 // place current date and time at the top of the page
+
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 // each time block is color-coded to indicate whether it is in the past, present, or future
 function changeColor(){
     let hour = moment().hours();
-console.log(hour)
+// console.log(hour)
+$(".time-block").each(function(){
+    let currentHour = parseInt($(this).attr("id"))
+ // console.log(this)
+ if (currentHour > hour) {
+     $(this).addClass("future");
+ }
+ else if (currentHour === hour){
+     $(this).addClass("present");
+ }
+ else {
+     $(this).addClass("past");
+ }
+})
+
+
+
 }
 changeColor()
+
