1f1c53a328288937f25890bf763e97567d480fcd
yourdilemma <k.iankovskaia@gmail.com>
2020-12-09 11:07:52 -0500

    added local time variable

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// place current date and time at the top of the page
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+// each time block is color-coded to indicate whether it is in the past, present, or future
+function changeColor(){
+    let hour = moment().hours();
+console.log(hour)
+}
+changeColor()
